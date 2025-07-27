a534ccd8760625bdceea08cb7a8c2423898b3886
shivam06010 <shivamsrivastava06010@gmail.com>
2025-07-27 11:02:13 +0530

    final 2.0

--- lib/auth.ts
+++ lib/auth.ts
@@ -75,7 +75,7 @@ export const authOptions: NextAuthOptions = {
 
     async session({ session, token }) {
       if (session.user) {
-        session.user.id = token.sub;
+        session.user.id = token.sub || "";
       }
       return session;
     },
